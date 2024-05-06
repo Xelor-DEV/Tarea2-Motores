b82e31259b55c28ab0924a596ea04c509f4c2697
Xelor-DEV <BladeUAOficial@gmail.com>
2024-05-05 23:54:36 -0500

    project completed to the best of our ability.

---
+++ Assets/Scripts/Game/UIManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text points;
+
+}
